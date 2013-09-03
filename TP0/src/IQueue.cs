e89427e8d58521ed8ac14b6098a798cb7ad0cb7b
Granjero <leandro_v_059@hotmail.com>
2013-09-03 15:39:54 -0300

    Actualizo .gitignore

---
+++ TP0/src/IQueue.cs
@@ -0,0 +1,11 @@
+﻿namespace TP0{
+
+    public interface IQueue{
+
+        bool IsEmpty { get; }
+        int Size { get; }
+        void Add(object item);
+        object Top { get; }
+        void Remove();
+    }
+}
